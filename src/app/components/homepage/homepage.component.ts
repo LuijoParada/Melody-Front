5c46a4bb298b85f2d0c40b63def86dea732806c0
LuijoParada <lujoparca@gmail.com>
2024-12-01 05:12:28 -0400

    batman aesthetics

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.css'
+  styleUrl: './homepage.component.css',
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
+  images = [
+    'carousel1.png', // Cambia estas rutas a las de tus imágenes
+    'carousel2.png',
+    'carousel3.png',
+    'carousel4.png',
+  ];
+  images2 = [
+    'carousel5.png', // Cambia estas rutas a las de tus imágenes
+    'carousel6.png',
+    'carousel7.png',
+    'carousel8.png',
+  ]
 
+  currentIndex = 0;
+  transition = 'transform 0.5s ease-in-out';
+
+  ngOnInit() {
+    this.startAutoSlide();
+  }
+
+  startAutoSlide() {
+    setInterval(() => {
+      this.nextSlide();
+    }, 4500);
+  }
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  getTransform(): string {
+    return `translateX(-${this.currentIndex * 100}%)`;
+  }
 }
