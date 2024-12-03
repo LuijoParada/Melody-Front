8f2951c9fa02c9c72e239b3c49c400f08ce9be97
LuijoParada <lujoparca@gmail.com>
2024-12-03 04:35:11 -0400

    batman mental meltdown

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
-import { TableModule } from 'primeng/table';
+import { Component, OnInit } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
+import { FavoritesService } from '../../services/favorites.service';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [TableModule, CommonModule,],
+  imports: [CommonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+  favorites: any[] = [];
+  userId: number = 32; // Aquí puedes setear el ID estático o dinámicamente
+
+  constructor(private favoritesService: FavoritesService) {}
+
+  ngOnInit(): void {
+    this.loadFavorites();
+  }
+
+  loadFavorites(): void {
+    this.favoritesService.getFavorites(this.userId).subscribe(
+      (data) => {
+        this.favorites = data;
+      },
+      (error) => {
+        console.error('Error fetching favorites:', error);
+      }
+    );
+  }
+
+  deleteFavorite(id: number): void {
+    // Implementar eliminación si es necesario
+    console.log('Delete favorite with id:', id);
+  }
 }
